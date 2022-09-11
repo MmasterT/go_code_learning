e6c74de9f57cefcf0dd4a7b753b4c7e23cc5fbfd
Mariano Olivera Fedi <marianooliverafedi@gmail.com>
2022-09-11 09:19:42 +0100

    add changes

--- excersies_cap1.go
+++ excersies_cap1.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -9,8 +16,11 @@ import (
 )
 
 func main() {
-	excercise_1()
-	excercise_2()
+	//excercise_1()
+	//excercise_2()
+	excercise_4()
+	excercise_5(os.Stdout)
+
 }
 
 func excercise_1() {
@@ -31,3 +41,73 @@ func excercise_2() {
 	secs := time.Since(start).Seconds()
 	fmt.Println(secs)
 }
+
+//excercise_3 is add the time module and do a benchmarrk of the functions
+
+// #TODO excersie_4
+func excercise_4() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	filename := ""
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s\t%s\t%d\n", filename, line, n)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0x99, 0x00, 0xff}}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func excercise_5(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 128   // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.05
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+// #TODO excersie_6
