906f6d5d4bbbf028c5b4d652959bf21f3fd9d122
Mariano Olivera Fedi <marianooliverafedi@gmail.com>
2022-09-17 17:44:08 +0100

    advanced in the book

--- chapter_2/excercises_chapter_2.go
+++ chapter_2/excercises_chapter_2.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
-	"tempconv"
 )
 
 func main() {
@@ -13,19 +13,63 @@ func main() {
 
 //Excercise 1 was creating the package tempconv (in this directory and $GOPAATH in my desktop)
 
-//todo excercise 2. Change Celisus and Kelvin types to feet and meters. Initialize the const values as in ../packages/tempconv.go.
+// excercise 2. Change Celisus and Kelvin types to feet and meters. Initialize the const values as in ../packages/tempconv.go.
 //Then make the progrram to read command line arguments and os.Stdin.
 
+type Meter float32
+type Feet float32
+
+func distM2F(m Meter) Feet {
+	return Feet(m * 3.28084)
+}
+
+func distF2M(f Feet) Meter {
+	return Meter(f / 3.28084)
+}
+
 func excercise_2() {
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
-			os.Exit(1)
+
+	if len(os.Args[1:]) != 0 {
+
+		for _, arg := range os.Args[1:] {
+			t, err := strconv.ParseFloat(arg, 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "mf: %v\n", err)
+				os.Exit(1)
+			}
+			m := Meter(t)
+			f := Feet(t)
+			fmt.Printf("%v = %v, %v = %v\n",
+				m, distM2F(m), f, distF2M(f))
 		}
-		k := tempconv.Kelvin(t)
-		c := tempconv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			k, tempconv.FToC(f), c, tempconv.CToF(c))
+
+	} else {
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			temp, _ := strconv.ParseFloat(input.Text(), 32)
+			m := Meter(temp)
+			f := Feet(temp)
+			fmt.Printf("%v = %v meters, %v = %v foots\n",
+				m, distM2F(m), f, distF2M(f))
+		}
+	}
+}
+
+//exercsie 3 done
+var sum byte
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+func excercise_3(){
+	
+	func PopCount( x uint64) int {
+		
+		
+		for i := range 8 {
+			sum = sum + pc[byte(x>>i*8)]
+		}
+		return int(sum)
 	}
 }
--- packages/tempconv.go
+++ packages/tempconv.go
@@ -15,18 +15,33 @@ const (
 	BoilingC      Celsius = 100
 )
 
-func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
-func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
+}
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%g°K", k)
+}
 
 // CToF converts a Celsius temperature to Fahrenheit.
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
 
 // FToC converts a Fahrenheit temperature to Celsius.
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
 
 // KToC converts a Kelvin temperature to Celsius.
-func KToC(k Kelvin) Celsius { return Celsius(k + 273.15) }
+func KToC(k Kelvin) Celsius {
+	return Celsius(k + 273.15)
+}
 
 // CToK converts a Celsius temperature to Kelvin.
-func CToK(c Celsius) Kelvin { return Kelvin(c - 273.15) }
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - 273.15)
+}
