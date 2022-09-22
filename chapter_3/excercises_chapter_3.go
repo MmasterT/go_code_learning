7d3d34bec2a244a71a39244d4ab9657eebd7ab43
Mariano Olivera Fedi <marianooliverafedi@gmail.com>
2022-09-22 17:15:47 +0100

    book exercises

--- chapter_3/excercises_chapter_3.go
+++ chapter_3/excercises_chapter_3.go
@@ -1,16 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"image"
-	"image/color"
-	"image/png"
 	"io"
 	"log"
 	"math"
-	"math/cmplx"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -32,7 +31,9 @@ func main() {
 	//excercise_3() //done in the web
 	//excercise_4()
 	//excercise_5() //done in the web
-	excercise_6() //todo 6-9
+	//excercise_6() //todo 6-9
+	//excercise_10() //excercise 10 and 11 together
+	excercise_12()
 }
 
 func excercise_2() {
@@ -113,6 +114,7 @@ func plot3d() string {
 	return print_out
 }
 
+/*
 func excercise_6() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
@@ -138,6 +140,7 @@ func Map[T, V any](ts []T, fn func(T) V) []V {
 	}
 	return result
 }
+
 
 func mandelbrot(z complex128) color.Color {
 	const (
@@ -179,4 +182,87 @@ func mandelbrot(z complex128) color.Color {
 		r |= r << 8
 	})
 	return color.Black //aca hay que devolver el promedio
+}
+*/
+
+// comma inserts commas in a non-negative decimal integer string.
+func excercise_10() {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSuffix(text, "\n")
+
+	fmt.Println(comma_1(text))
+}
+
+func comma_1(s string) string {
+	var buf bytes.Buffer
+
+	if strings.Contains(s, ".") {
+		index := strings.Index(s, ".")
+		s = s[0:index]
+	}
+
+	n := len(s)
+
+	if n <= 3 {
+		return s
+	}
+	for i := 0; i < n; i++ {
+
+		if (n-i)%3 == 0 {
+			buf.WriteString(",")
+		}
+		fmt.Fprintf(&buf, "%v", string(s[i]))
+	}
+	return buf.String()
+}
+
+func excercise_12() {
+	s1 := os.Args[1]
+	s2 := os.Args[2]
+	fmt.Printf("%s , %s\n", s1, s2)
+	if anagram(s1, s2) {
+		fmt.Println("You are in the presence of an anagram!")
+	} else {
+		fmt.Println("no anagram but nice dick, bro!")
+	}
+
+}
+
+func anagram(s1 string, s2 string) bool {
+	s1 = strings.TrimSpace(s1)
+	s2 = strings.TrimSpace(s2)
+
+	if len(s1) != len(s2) {
+		return false
+	}
+	r1 := []rune(s1)
+	r2 := []rune(s2)
+	for i, value_r1 := range s1 {
+		for j, value_r2 := range s2 {
+
+			if value_r1 == value_r2 {
+				r1 = delChar(r1, i)
+				r2 = delChar(r2, j)
+				fmt.Println("deleting a pair of chars...")
+			}
+			s1 = string(r1)
+			s2 = string(r2)
+			fmt.Println("searching anagram...")
+			if len(r1) != len(r2) && r1 != nil && r2 != nil {
+				fmt.Println("different length, aha!")
+				return false
+			}
+		}
+
+	}
+	return true
+}
+
+func delChar(s []rune, index int) []rune {
+	if index < len(s)-1 {
+		return append(s[0:index], s[index+1:]...)
+	} else {
+		return append(s[0:])
+	}
 }
