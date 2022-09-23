6161fac24fb99ae4bba2d0361a82cb4ef51d88da
Mariano Olivera Fedi <marianooliverafedi@gmail.com>
2022-09-23 09:23:48 +0100

    start of chapter 4

---
+++ chapter_4/3_equalsice.go
@@ -0,0 +1,14 @@
+package main
+
+// example comparasion between slices (onlline for same type)
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
